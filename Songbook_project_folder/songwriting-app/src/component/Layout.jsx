ee296f94289fb213bfaea5109e70156dd1cd5a2c
avemufame <avem00@yahoo.it>
2026-08-20 02:46:07 +0100

    new Layout working in progress

--- Songbook_project_folder/songwriting-app/src/component/Layout.jsx
+++ Songbook_project_folder/songwriting-app/src/component/Layout.jsx
@@ -89,7 +89,9 @@ export default function Layout() {
 
 
           {/* 3. RIGHT SIDEBAR (The Aside Drawer) */}
+
           <AppShell.Aside p="md">
+            
         <Text fw={700} size="sm" mb="md" c="violet">SAve Open new SOng pannel </Text>
         <Text size="xs" c="dimmed" mb="sm">Save song/open song :</Text>
         <Text size="sm" fw={500}>• Working in progress</Text>
